58162e5a1d27c7f355ffd2a7d3b7139305b046a9
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-30 21:40:02 +0200

    Second commit

--- src/pages/repl.tsx
+++ src/pages/repl.tsx
@@ -1,1 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from '../containers';
 import '../styles/styles.css';
+
+ReactDOM.render(<App />, document.getElementById('app'));
