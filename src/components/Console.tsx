c8aab0286ea06b20e7308606e3972e2bfce5e1eb
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-30 22:16:59 +0200

    Add base Console component and App container.

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -1,7 +1,36 @@
 import * as React from 'react';
 
-export default (props: {}): React.ReactElement<{}> => (
-  <div className="console">
-    <div>Console</div>
-  </div>
-);
+interface ILogItem {
+  type: 'input' | 'output' | 'error',
+  content: string,
+}
+
+// interface IConsole {
+//   log: ILogItem[],
+//   onLine: (content: string) => boolean,
+// }
+
+interface IConsoleState {
+  input: string,
+  cursorPosition: number,
+  log: ILogItem[],
+}
+
+export default class Console extends React.Component<{}, IConsoleState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      input: '',
+      cursorPosition: Infinity,
+      log: [],
+    };
+  }
+
+  public render() {
+    return (
+      <div className="console">
+      </div>
+    );
+  }
+};
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { Console } from '../components';
 
 export default class App extends React.Component<{}, {}> {
+  constructor(props: {}) {
+    super(props);
+  }
+
   public render() {
     return <Console />;
   }
