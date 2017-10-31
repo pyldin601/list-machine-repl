94e3aa4232fe53921e6725c8130a3a13c60b44d2
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-31 11:45:28 +0200

    Create basic terminal component

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 
 interface ILogItem {
   type: 'input' | 'output' | 'error',
@@ -17,19 +18,99 @@ interface IConsoleState {
 }
 
 export default class Console extends React.Component<{}, IConsoleState> {
+  private consoleRef: HTMLDivElement;
+
   constructor(props: {}) {
     super(props);
 
     this.state = {
       input: '',
-      cursorPosition: Infinity,
+      cursorPosition: 0,
       log: [],
     };
+
+    this.renderLogItem = this.renderLogItem.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+  }
+
+  public onKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
+    console.log(event.key);
+    switch (event.key) {
+      case 'ArrowLeft':
+        return this.onArrowLeftKeyPressed();
+      case 'ArrowRight':
+        return this.onArrowRightKeyPressed();
+    }
+  }
+
+  public onArrowLeftKeyPressed() {
+    this.setState({
+      cursorPosition: Math.max(0, this.state.cursorPosition - 1),
+    })
+  }
+
+  public onArrowRightKeyPressed() {
+    this.setState({
+      cursorPosition: Math.min(this.state.input.length, this.state.cursorPosition + 1),
+    })
+  }
+
+  public onKeyPress(event: React.KeyboardEvent<HTMLDivElement>) {
+    const beforeCursor = this.state.input.slice(0, this.state.cursorPosition);
+    const afterCursor = this.state.input.slice(this.state.cursorPosition);
+    this.setState({
+      input: `${beforeCursor}${event.key}${afterCursor}`,
+      cursorPosition: this.state.cursorPosition + 1,
+    })
+  }
+
+  public componentDidMount() {
+    this.focus();
+  }
+
+  public focus() {
+    this.consoleRef.focus();
+  }
+
+  public renderLogItem(item: ILogItem, key: any) {
+    return <span key={key}>{item.content}</span>;
+  }
+
+  public renderLog() {
+    return (
+      <div className="log">
+        {this.state.log.map(this.renderLogItem)}
+      </div>
+    );
+  }
+
+  public renderPrompt() {
+    const currentInput = this.state.input;
+    const inputBeforeCursor = currentInput.slice(0, this.state.cursorPosition);
+    const inputAtCursor = currentInput[this.state.cursorPosition] || ' ';
+    const inputAfterCursor = currentInput.slice(this.state.cursorPosition + 1);
+
+    return (
+      <div className="prompt">
+        <span>{inputBeforeCursor}</span>
+        <span className="cursor">{inputAtCursor}</span>
+        <span>{inputAfterCursor}</span>
+      </div>
+    );
   }
 
   public render() {
     return (
-      <div className="console">
+      <div
+        ref={ref => this.consoleRef = ref}
+        className="console"
+        tabIndex={0}
+        onKeyDown={this.onKeyDown}
+        onKeyPress={this.onKeyPress}
+      >
+        {this.renderLog()}
+        {this.renderPrompt()}
       </div>
     );
   }
