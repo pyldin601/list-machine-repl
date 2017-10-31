40d1b1c4b646e147fe69b6e3ef22580ef5364f5a
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-31 12:41:57 +0200

    Add many keys handlers

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -34,6 +34,10 @@ export default class Console extends React.Component<{}, IConsoleState> {
     this.onKeyPress = this.onKeyPress.bind(this);
   }
 
+  public setStateAsync = (state: any) => new Promise((resolve) => {
+    this.setState(state, resolve);
+  });
+
   public onKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
     console.log(event.key);
     switch (event.key) {
@@ -41,28 +45,99 @@ export default class Console extends React.Component<{}, IConsoleState> {
         return this.onArrowLeftKeyPressed();
       case 'ArrowRight':
         return this.onArrowRightKeyPressed();
+      case 'End':
+        return this.onEndKeyPressed();
+      case 'Home':
+        return this.onHomePressed();
+      case 'Backspace':
+        return this.onBackspacePressed();
+      case 'Delete':
+        return this.onDeletePressed();
     }
   }
 
   public onArrowLeftKeyPressed() {
-    this.setState({
+    return this.setStateAsync({
       cursorPosition: Math.max(0, this.state.cursorPosition - 1),
     })
   }
 
   public onArrowRightKeyPressed() {
-    this.setState({
-      cursorPosition: Math.min(this.state.input.length, this.state.cursorPosition + 1),
+    return this.setStateAsync({
+      cursorPosition: Math.min(
+        this.state.input.length,
+        this.state.cursorPosition + 1,
+      ),
+    })
+  }
+
+  public onEndKeyPressed() {
+    return this.setStateAsync({
+      cursorPosition: this.state.input.length,
+    })
+  }
+
+  public onHomePressed() {
+    return this.setStateAsync({
+      cursorPosition: 0,
+    })
+  }
+
+  public onBackspacePressed() {
+    const beforeCursor = this.state.input.slice(0, this.state.cursorPosition);
+    const afterCursor = this.state.input.slice(this.state.cursorPosition);
+
+    return this.setStateAsync({
+      input: `${beforeCursor.slice(0, -1)}${afterCursor}`,
+      cursorPosition: Math.max(0, this.state.cursorPosition - 1),
+    })
+  }
+
+  public onDeletePressed() {
+    const beforeCursor = this.state.input.slice(0, this.state.cursorPosition);
+    const afterCursor = this.state.input.slice(this.state.cursorPosition);
+
+    return this.setStateAsync({
+      input: `${beforeCursor}${afterCursor.slice(1)}`,
+    })
+  }
+
+  public async onEnterPressed() {
+    const input = this.state.input;
+    await this.writeLine('input', `${input}\n`);
+    await this.writeLine('output', 'OK\n');
+    await this.cleanInput();
+  }
+
+  public writeLine(type: ILogItem['type'], content: ILogItem['content']) {
+    return this.setStateAsync({
+      log: [...this.state.log, { type, content }],
+    });
+  }
+
+  public cleanInput() {
+    return this.setStateAsync({
+      input: '',
+      cursorPosition: 0,
     })
   }
 
   public onKeyPress(event: React.KeyboardEvent<HTMLDivElement>) {
-    const beforeCursor = this.state.input.slice(0, this.state.cursorPosition);
-    const afterCursor = this.state.input.slice(this.state.cursorPosition);
-    this.setState({
-      input: `${beforeCursor}${event.key}${afterCursor}`,
-      cursorPosition: this.state.cursorPosition + 1,
-    })
+    switch (event.key) {
+      case 'Enter':
+        return this.onEnterPressed();
+
+      default: {
+        const beforeCursor = this.state.input.slice(0, this.state.cursorPosition);
+        const afterCursor = this.state.input.slice(this.state.cursorPosition);
+
+        return this.setStateAsync({
+          input: `${beforeCursor}${event.key}${afterCursor}`,
+          cursorPosition: this.state.cursorPosition + 1,
+        })
+      }
+    }
+
   }
 
   public componentDidMount() {
